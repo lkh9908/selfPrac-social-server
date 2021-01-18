474ddd4aa9a3831e29f8de753222a55575650c9d
kaihao liu <yubanmeiqin9908@gmail.com>
2021-01-18 14:05:19 -0500

    changed git ignore, included config

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGODB: 
+        'mongodb+srv://lkh:liukaihao9908@cluster0.xzwo4.mongodb.net/test_database?retryWrites=true&w=majority',
+    SECRET_KEY: 
+        'some very secret key'
+}
