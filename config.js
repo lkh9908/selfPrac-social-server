dddcc6991d591621f2d24e8588baf3f51f1b9121
Kaihao Liu <46586250+lkh9908@users.noreply.github.com>
2021-02-04 15:43:48 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    MONGODB: 
-        'mongodb+srv://lkh:liukaihao9908@cluster0.xzwo4.mongodb.net/test_database?retryWrites=true&w=majority',
-    SECRET_KEY: 
-        'some very secret key'
-}
